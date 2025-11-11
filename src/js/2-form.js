e2e3d3e2ed2040d9509788932d40e30f53ab0a3e
Maksym Lutsiak <demon164320@gmail.com>
2025-11-11 13:40:25 +0100

    i

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log('Form page loaded');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+    email: '',
+    message: '',
+};
+
+
+const form = document.querySelector('.feedback-form');
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    try {
+    formData = JSON.parse(savedData);
+
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+    } catch (error) {
+    console.error('Помилка при зчитуванні даних:', error);
+    }
+}
+
+form.addEventListener('input', event => {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+
+    if (!formData.email || !formData.message) {
+    alert('Будь ласка, заповніть усі поля.');
+    return;
+    }
+
+    console.log('Submitted form data:', formData);
+
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = { email: '', message: '' };
+});
